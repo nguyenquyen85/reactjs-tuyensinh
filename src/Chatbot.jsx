443ae7d1061853b833436fecd7eee09455f54dea
nguyenquyen-coder <quyen0012341982@gmail.com>
2025-05-12 09:53:11 +0700

    chỉnh sử

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -22,7 +22,7 @@ const Chatbot = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("https://rasa-tuyensinh.onrender.com/webhooks/rest/webhook", {
+      const response = await fetch("http://localhost:5005/webhooks/rest/webhook", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
