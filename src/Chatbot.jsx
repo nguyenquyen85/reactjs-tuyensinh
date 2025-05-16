75afd4b92bda60320d8fcda612dc8860a328f32f
nguyenquyen-coder <quyen0012341982@gmail.com>
2025-05-16 13:11:13 +0700

    cập nhật api

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Chatbot.css";
+const RASA_URL = import.meta.env.VITE_RASA_URL;
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([
@@ -22,7 +23,7 @@ const Chatbot = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("https://2aff-116-110-42-18.ngrok-free.app/webhooks/rest/webhook", {
+      const response = await fetch(`${RASA_URL}/webhooks/rest/webhook`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
